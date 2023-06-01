197bb60fffdf7b7e54b852ebdef762f2cbddc862
Oplala <oplala2020@gmail.com>
2023-06-01 23:02:33 +0530

    updated files

--- js/clint.js
+++ js/clint.js
@@ -3,19 +3,19 @@ const socket = io('http://localhost:8000');
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp')
 const messageContainer = document.querySelector(".container")
-var audio = new Audio('Msg rcv.mp3');
+var audio = new Audio('templates/Msg rcv.mp3');
 
-const append = (message, position)=>{
+const append = (message, position) => {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
-    if(position =='left'){
+    if (position == 'left') {
         audio.play();
     }
-} 
-form.addEventListener('submit', (e)=>{
+}
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
     append(`you: ${message}`, 'right');
@@ -23,18 +23,18 @@ form.addEventListener('submit', (e)=>{
     messageInput.value = ''
 })
 const name = prompt("Enter your name to join");
-socket.emit('new-user-joined',name);
+socket.emit('new-user-joined', name);
 
-socket.on('user-joined', name =>{
+socket.on('user-joined', name => {
     append(`${name} joined the chat!`, 'right')
 
 })
-socket.on('receive', data =>{
+socket.on('receive', data => {
     append(`${data.name}: ${data.message}`, 'left')
 
 })
 
-socket.on('left', name =>{
+socket.on('left', name => {
     append(`${name} left the chat!`, 'left')
 
 })
